aee741a039bb509409063fc2861411c00dabad3f
Andrej Milanović <amilanovi0@gmail.com>
2025-10-05 18:52:58 +0200

    chore: update eslint configuration to include eslint-plugin-import

--- eslint.config.js
+++ eslint.config.js
@@ -2,17 +2,41 @@ import eslint from '@eslint/js'
 import { defineConfig, globalIgnores } from 'eslint/config'
 import tseslint from 'typescript-eslint'
 import prettierConfig from 'eslint-config-prettier'
+import importPlugin from 'eslint-plugin-import'
 
 export default defineConfig(
   globalIgnores(['dist/', 'eslint.config.js']),
   eslint.configs.recommended,
   tseslint.configs.recommendedTypeChecked,
+  importPlugin.flatConfigs.recommended,
+  importPlugin.flatConfigs.typescript,
   {
     languageOptions: {
       parserOptions: {
         projectService: true,
         tsconfigRootDir: import.meta.dirname,
       },
+    },
+    rules: {
+      'import/order': [
+        'error',
+        {
+          groups: [
+            'builtin',
+            'external',
+            'internal',
+            ['sibling', 'parent'],
+            'index',
+          ],
+          'newlines-between': 'never',
+          distinctGroup: false,
+          alphabetize: {
+            order: 'asc',
+            caseInsensitive: true,
+          },
+        },
+      ],
+      'import/newline-after-import': 'error',
     },
   },
   prettierConfig,
