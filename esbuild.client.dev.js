ad88231d25baf9c431af64200258852805d5946c
Andrej Milanović <amilanovi0@gmail.com>
2026-01-04 19:06:52 +0100

    feat: add simple favicon
    Added a simple favicon that changes to light or dark based on system appearance

--- esbuild.client.dev.js
+++ esbuild.client.dev.js
@@ -11,10 +11,26 @@ async function buildHTML(result) {
     result.outputFiles.find((file) => file.path.endsWith('.css'))?.text || ''
 
   const html = await readFile('src/client/index.html', 'utf-8')
+  const lightFavicon = await readFile(
+    'src/client/favicon/light-favicon.svg',
+    'utf-8',
+  )
+  const darkFavicon = await readFile(
+    'src/client/favicon/dark-favicon.svg',
+    'utf-8',
+  )
 
   let output = html
   output = output.replace('<!-- STYLE_OUTLET -->', `<style>${css}</style>`)
   output = output.replace('<!-- SCRIPT_OUTLET -->', `<script>${js}</script>`)
+  output = output.replace(
+    /href="favicon\/light-favicon\.svg"/,
+    `href="data:image/svg+xml,${encodeURIComponent(lightFavicon)}"`,
+  )
+  output = output.replace(
+    /href="favicon\/dark-favicon\.svg"/,
+    `href="data:image/svg+xml,${encodeURIComponent(darkFavicon)}"`,
+  )
 
   await mkdir('.dev-build/client', { recursive: true })
   await writeFile('.dev-build/client/index.html', output)
--- esbuild.client.js
+++ esbuild.client.js
@@ -21,10 +21,26 @@ const css =
   result.outputFiles.find((file) => file.path.endsWith('.css'))?.text || ''
 
 const html = await readFile('src/client/index.html', 'utf-8')
+const lightFavicon = await readFile(
+  'src/client/favicon/light-favicon.svg',
+  'utf-8',
+)
+const darkFavicon = await readFile(
+  'src/client/favicon/dark-favicon.svg',
+  'utf-8',
+)
 
 let output = html
 output = output.replace('<!-- STYLE_OUTLET -->', `<style>${css}</style>`)
 output = output.replace('<!-- SCRIPT_OUTLET -->', `<script>${js}</script>`)
+output = output.replace(
+  /href="favicon\/light-favicon\.svg"/,
+  `href="data:image/svg+xml,${encodeURIComponent(lightFavicon)}"`,
+)
+output = output.replace(
+  /href="favicon\/dark-favicon\.svg"/,
+  `href="data:image/svg+xml,${encodeURIComponent(darkFavicon)}"`,
+)
 
 await mkdir('dist/client', { recursive: true })
 await writeFile('dist/client/index.html', output)
